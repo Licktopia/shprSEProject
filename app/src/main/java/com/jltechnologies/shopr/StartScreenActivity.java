bd5a4edb5e7266f3a4a0910e46d57b13719e03e9
blazerzero <habibelo@oregonstate.edu>
2017-06-08 08:53:32 -0700

    Almost complete default store functionality.

--- app/src/main/java/com/jltechnologies/shopr/StartScreenActivity.java
+++ app/src/main/java/com/jltechnologies/shopr/StartScreenActivity.java
@@ -4,11 +4,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class StartScreenActivity extends AppCompatActivity {
     private int[] storeTags = {R.id.store1,R.id.store2,R.id.store3,R.id.store4,R.id.store5, R.id.store6};//array that holds the ids of the store text views
     private TextView[] storeButtons;//array of store text views
+    public static boolean store1Default = true;
+    public static boolean store2Default = false;
+    public static boolean store3Default = false;
+    public static boolean store4Default = false;
+    public static boolean store5Default = false;
+    public static boolean store6Default = false;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +39,74 @@ public class StartScreenActivity extends AppCompatActivity {
            });
         }
 
+    }
+
+    public void onRadioButtonClicked(View view) {//Select Left or Right Entrance
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.default_radio1:
+                if (checked) {
+                    store1Default = true;
+                    store2Default = false;
+                    store3Default = false;
+                    store4Default = false;
+                    store5Default = false;
+                    store6Default = false;
+                    break;
+                }
+            case R.id.default_radio2:
+                if (checked) {
+                    store1Default = false;
+                    store2Default = true;
+                    store3Default = false;
+                    store4Default = false;
+                    store5Default = false;
+                    store6Default = false;
+                    break;
+                }
+            case R.id.default_radio3:
+                if (checked) {
+                    store1Default = false;
+                    store2Default = false;
+                    store3Default = true;
+                    store4Default = false;
+                    store5Default = false;
+                    store6Default = false;
+                    break;
+                }
+            case R.id.default_radio4:
+                if (checked) {
+                    store1Default = false;
+                    store2Default = false;
+                    store3Default = false;
+                    store4Default = true;
+                    store5Default = false;
+                    store6Default = false;
+                    break;
+                }
+            case R.id.default_radio5:
+                if (checked) {
+                    store1Default = false;
+                    store2Default = false;
+                    store3Default = false;
+                    store4Default = false;
+                    store5Default = true;
+                    store6Default = false;
+                    break;
+                }
+            case R.id.default_radio6:
+                if (checked) {
+                    store1Default = false;
+                    store2Default = false;
+                    store3Default = false;
+                    store4Default = false;
+                    store5Default = false;
+                    store6Default = true;
+                    break;
+                }
+        }
     }
 }
